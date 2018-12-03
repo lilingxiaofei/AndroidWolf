95a04f8b0d92d09c8a00321aa0fe808cb023ebbd
wushenfei <wusf_dafei@163.com>
2018-12-03 21:06:51 +0800

    增加粉丝模块功能能

---
+++ app/src/main/java/com/chunlangjiu/app/fans/activity/FansInfoActivity.java
@@ -0,0 +1,20 @@
+package com.chunlangjiu.app.fans.activity;
+
+import android.os.Bundle;
+
+import com.chunlangjiu.app.R;
+import com.chunlangjiu.app.abase.BaseActivity;
+
+public class FansInfoActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fans_activity_home);
+    }
+
+    @Override
+    public void setTitleView() {
+        hideTitleView();
+    }
+}
