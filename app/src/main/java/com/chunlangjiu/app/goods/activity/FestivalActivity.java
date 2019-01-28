dc83dd590c2db5572dcac2425fd406766106970c
wushenfei <wusf_dafei@163.com>
2019-01-28 20:57:41 +0800

    修改节日活动页面

--- app/src/main/java/com/chunlangjiu/app/goods/activity/FestivalActivity.java
+++ app/src/main/java/com/chunlangjiu/app/goods/activity/FestivalActivity.java
@@ -21,6 +21,9 @@ import com.chad.library.adapter.base.BaseViewHolder;
 import com.chunlangjiu.app.R;
 import com.chunlangjiu.app.abase.BaseActivity;
 import com.chunlangjiu.app.abase.BaseApplication;
+import com.chunlangjiu.app.amain.bean.HomeAuctionBean;
+import com.chunlangjiu.app.amain.bean.HomeBean;
+import com.chunlangjiu.app.amain.bean.HomeListBean;
 import com.chunlangjiu.app.amain.bean.ThirdClassBean;
 import com.chunlangjiu.app.goods.adapter.GoodsAdapter;
 import com.chunlangjiu.app.goods.bean.AlcListBean;
@@ -116,8 +119,8 @@ public class FestivalActivity extends BaseActivity {
 
 
     private void initView() {
-        MyStatusBarUtils.setStatusBar(this,ContextCompat.getColor(this, R.color.bg_red));
-        MyStatusBarUtils.setFitsSystemWindows(findViewById(R.id.llRootLayout),true);
+        MyStatusBarUtils.setStatusBar(this, ContextCompat.getColor(this, R.color.bg_red));
+        MyStatusBarUtils.setFitsSystemWindows(findViewById(R.id.llRootLayout), true);
 
         lists = new ArrayList<>();
         goodsAdapter = new GoodsAdapter(this, lists);
@@ -126,7 +129,7 @@ public class FestivalActivity extends BaseActivity {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
                 GoodsListDetailBean details = lists.get(position);
-                GoodsDetailslNewActivity.startActivity(FestivalActivity.this,details.getItem_id());
+                GoodsDetailslNewActivity.startActivity(FestivalActivity.this, details.getItem_id());
             }
         });
 
@@ -142,6 +145,99 @@ public class FestivalActivity extends BaseActivity {
         disposable = new CompositeDisposable();
     }
 
+    private void getHomeList() {
+        disposable.add(ApiUtils.getInstance().getHomeLists(1)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<ResultBean<HomeListBean>>() {
+                    @Override
+                    public void accept(ResultBean<HomeListBean> homeListBeanResultBean) throws Exception {
+//                        getListSuccess(homeListBeanResultBean);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+                    }
+                }));
+    }
+
+//    private void getListSuccess(ResultBean<HomeListBean> homeListBeanResultBean) {
+//        List<HomeBean> newLists = homeListBeanResultBean.getData().getList();
+//        List<HomeAuctionBean> auction_list = homeListBeanResultBean.getData().getAuction_list();
+//        if (newLists == null) newLists = new ArrayList<>();
+//        if (auction_list == null) auction_list = new ArrayList<>();
+//        for (HomeBean homeBean : newLists) {
+//            homeBean.setItemType(HomeBean.ITEM_GRID_GOODS);
+//            homeBean.setAuction(false);
+//        }
+//        if (auction_list != null && auction_list.size() > 0) {
+//            lists.clear();
+//            if (BaseApplication.HIDE_AUCTION) {
+//                auction_list.clear();
+//            } else {
+//                HomeBean jingpaiFlag = new HomeBean();
+//                jingpaiFlag.setItemType(HomeBean.ITEM_JINGPAI);
+//                lists.add(jingpaiFlag);
+//                for (int i = 0; i < auction_list.size(); i++) {
+//                    HomeAuctionBean auctionBean = auction_list.get(i);
+//                    String end_time = auctionBean.getAuction_end_time();
+//                    long endTime = 0;
+//                    if (!TextUtils.isEmpty(end_time)) {
+//                        endTime = Long.parseLong(end_time);
+//                    }
+//                    if ((endTime * 1000 - System.currentTimeMillis()) > 0) {
+//                        HomeBean homeBean = new HomeBean();
+//                        homeBean.setItemType(HomeBean.ITEM_GOODS);
+//                        homeBean.setAuction(true);
+//                        homeBean.setItem_id(auctionBean.getItem_id());
+//                        homeBean.setTitle(auctionBean.getTitle());
+//                        homeBean.setImage_default_id(auctionBean.getImage_default_id());
+//                        homeBean.setLabel(auctionBean.getLabel());
+//                        homeBean.setAuction_starting_price(auctionBean.getAuction_starting_price());
+//                        homeBean.setAuction_end_time(auctionBean.getAuction_end_time());
+//                        homeBean.setMax_price(auctionBean.getMax_price());
+//                        homeBean.setAuction_status(auctionBean.getAuction_status());
+//                        homeBean.setAuction_number(auctionBean.getAuction_number());
+//
+//                        homeBean.setShop_id(auctionBean.getShop_id());
+//                        homeBean.setShop_name(auctionBean.getShop_name());
+//                        homeBean.setGrade(auctionBean.getGrade());
+//                        homeBean.setView_count(auctionBean.getView_count());
+//                        homeBean.setRate_count(auctionBean.getRate_count());
+//                        homeBean.setRate(auctionBean.getRate());
+//                        lists.add(homeBean);
+//                    }
+//                }
+//            }
+//
+//            if (newLists.size() > 0) {
+//                HomeBean tuijian = new HomeBean();
+//                tuijian.setItemType(HomeBean.ITEM_TUIJIAN);
+//                lists.add(tuijian);
+//            }
+//            lists.addAll(newLists);
+//        } else {
+//            lists.clear();
+//            if (newLists.size() > 0) {
+//                HomeBean tuijian = new HomeBean();
+//                tuijian.setItemType(HomeBean.ITEM_TUIJIAN);
+//                lists.add(tuijian);
+//            }
+//            lists.addAll(newLists);
+//        }
+//        if (lists.size() == 0) {
+//            refreshLayout.setEnableLoadMore(false);//设置不能加载更多
+//        } else {
+//            refreshLayout.setEnableLoadMore(true);//设置能加载更多
+//        }
+//        if (lists.size() < 10) {
+//            refreshLayout.setFooterHeight(30);
+//            refreshLayout.finishLoadMoreWithNoMoreData();//显示没有更多数据
+//        } else {
+//            refreshLayout.setNoMoreData(false);
+//        }
+//        homeAdapter.setNewData(lists);
+//    }
 
     protected void onDestroy() {
         super.onDestroy();
