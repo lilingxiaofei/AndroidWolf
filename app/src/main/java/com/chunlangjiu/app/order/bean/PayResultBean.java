31f75bd572817dc35e9fff04ab35b59031221e7b
wushenfei <wusf_dafei@163.com>
2019-02-22 15:18:50 +0800

    提交支付结果bean

---
+++ app/src/main/java/com/chunlangjiu/app/order/bean/PayResultBean.java
@@ -0,0 +1,17 @@
+package com.chunlangjiu.app.order.bean;
+
+/**
+ * Created by Administrator on 2019/2/21.
+ */
+
+public class PayResultBean {
+    private String status ;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
